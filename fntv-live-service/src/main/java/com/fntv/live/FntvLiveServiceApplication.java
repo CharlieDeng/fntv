cbda223bfd41ab01944861c9c9449d606e4b0870
Charlie Deng <charlie.deng@outlook.com>
2017-07-09 16:57:09 +0800

    added live service

---
+++ fntv-live-service/src/main/java/com/fntv/live/FntvLiveServiceApplication.java
@@ -0,0 +1,12 @@
+package com.fntv.live;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FntvLiveServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FntvLiveServiceApplication.class, args);
+	}
+}
